629cdffe6dd22fbc996ec08422b8b836e798fbb8
Mr-Desperado-19 <mr.arturhlebov@gmail.com>
2023-04-17 22:45:06 +0200

    Task 1 - Update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,27 +1,27 @@
 const startBtn = document.querySelector('[data-start]');
-    const stopBtn = document.querySelector('[data-stop]');
-    let intervalId;
+const stopBtn = document.querySelector('[data-stop]');
+let intervalId;
 
-    function getRandomHexColor() {
-      return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-    }
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
-    function startChangingColor() {
-      intervalId = setInterval(() => {
-        document.body.style.backgroundColor = getRandomHexColor();
-      }, 1000);
+function startChangingColor() {
+    intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
 
-      startBtn.disabled = true;
-      stopBtn.disabled = false;
-    }
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+}
 
-    function stopChangingColor() {
-      clearInterval(intervalId);
+function stopChangingColor() {
+    clearInterval(intervalId);
 
-      startBtn.disabled = false;
-      stopBtn.disabled = true;
-    }
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+}
 
-    startBtn.addEventListener('click', startChangingColor);
-    stopBtn.addEventListener('click', stopChangingColor);
+startBtn.addEventListener('click', startChangingColor);
+stopBtn.addEventListener('click', stopChangingColor);
 
