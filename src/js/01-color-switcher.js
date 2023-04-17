cef48d780afd2615b023ff5c29904b77b7ebb9ab
Mr-Desperado-19 <mr.arturhlebov@gmail.com>
2023-04-17 22:20:07 +0200

    Task 1 - Done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('[data-start]');
+    const stopBtn = document.querySelector('[data-stop]');
+    let intervalId;
+
+    function getRandomHexColor() {
+      return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+    }
+
+    function startChangingColor() {
+      intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+
+      startBtn.disabled = true;
+      stopBtn.disabled = false;
+    }
+
+    function stopChangingColor() {
+      clearInterval(intervalId);
+
+      startBtn.disabled = false;
+      stopBtn.disabled = true;
+    }
+
+    startBtn.addEventListener('click', startChangingColor);
+    stopBtn.addEventListener('click', stopChangingColor);
 
